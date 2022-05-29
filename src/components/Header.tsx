4e5afdc44a019d57a1b091c6162d85e7c07de552
Alexander <94293225+frontcursed@users.noreply.github.com>
2022-05-29 16:56:07 +0300

    12 lesson

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+    <img
+        src='https://www.adobe.com/express/create/media_127a4cd0c28c2753638768caf8967503d38d01e4c.jpeg?width=400&format=jpeg&optimize=medium'/>
+        </header>;
+}
+
+export default Header;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img
+                src='https://media.istockphoto.com/photos/picturesque-morning-in-plitvice-national-park-colorful-spring-scene-picture-id1093110112?k=20&m=1093110112&s=612x612&w=0&h=3OhKOpvzOSJgwThQmGhshfOnZTvMExZX2R91jNNStBY='/>
+        </div>
+        <div>
+            ava + description
+        </div>
+        <div>
+            My posts
+        </div>
+        <div>
+            New post
+        </div>
+        <div>
+            <div>
+                post 1
+            </div>
+            <div>
+                post 2
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile;
